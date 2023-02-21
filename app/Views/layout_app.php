48e83930d7cd9de7827d8cf9a0b7ff3ce7a69049
hendikaseptio <hendikaseptio11@gmail.com>
2023-02-21 23:23:33 +0700

    membuat layout untuk pengguna

---
+++ app/Views/layout_app.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inpo Loker</title>
+	<link rel="stylesheet" href="/assets/css/custom.css">
+	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div class="container">
+				<a class="navbar-brand" href="#"><i class="fa fa-handshake"></i> Inpo Loker</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<a class="nav-link active" aria-current="page" href="/">Cari Lowongan Kerja</a>
+						</li>
+					</ul>
+					<div class="d-flex">
+						<button class="btn btn-outline-light" type="submit"><i class="fa fa-sign-in-alt"></i> Masuk</button>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<section id="banner">
+		<div class="container h-100">
+			<div class="d-flex h-100 align-items-center text-white">
+				<div class="col-6 m-auto text-center">
+					<h1>Dapatkan pekerjaan yang kamu impikan !</h1>
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt beatae labore adipisci veniam aperiam ipsa dolorem est deleniti in voluptatum temporibus corporis eveniet laborum tempora corrupti, accusantium, recusandae praesentium rem.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="loker" class="py-5">
+		<div class="container">
+			<h1>Lowongan Kerja</h1>
+			<p>Kami Menyediakan Berbagai Macam Lowongan Kerja Buat Kamu</p>
+			<div class="card shadow">
+				<div class="card-body">
+					<p>Cari Loker berdasarkan perusahaan atau nama pekerjaan</p>
+					<form action="">
+						<div class="row">
+							<div class="col-5">
+								<input type="text" class="form-control" placeholder="First name" aria-label="First name">
+							</div>
+							<div class="col-5">
+								<input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+							</div>
+							<div class="col-2">
+								<div class="d-grid">
+									<button class="btn btn-primary"><i class="fa fa-search"></i> Cari</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="row mt-5">
+				<div class="col-md-4 col-sm-6">
+					<div class="card shadow">
+						<div class="card-body">
+							<img src="/img/brand/pertamina.jpeg" class="img-fluid" alt="" width="70px" height="70px">
+							<h5 class="text-primary mt-3">Staf Administrasi</h5>
+							<p>PT.Pertamina</p>
+							<p>Lorem, ipsum dolor sit, amet consectetur adipisicing elit. Ad illum, corporis temporibus odit unde eius.</p>
+							<a href="" class="btn btn-primary"><i class="fa fa-folder-open"></i> Detail</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6">
+					<div class="card shadow">
+						<div class="card-body">
+							<img src="/img/brand/pertamina.jpeg" class="img-fluid" alt="" width="70px" height="70px">
+							<h5 class="text-primary mt-3">Staf Administrasi</h5>
+							<p>PT.Pertamina</p>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam, quis dolorem ad et dolores, animi.</p>
+							<a href="" class="btn btn-primary"><i class="fa fa-folder-open"></i> Detail</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6">
+					<div class="card shadow">
+						<div class="card-body">
+							<img src="/img/brand/shopeefood.png" class="img-fluid" alt="" width="70px" height="70px">
+							<h5 class="text-primary mt-3">Programmer Front End</h5>
+							<p>PT.Shoppe Indonesia</p>
+							<p>Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Autem perspiciatis doloribus reiciendis officiis aperiam quibusdam!</p>
+							<a href="" class="btn btn-primary"><i class="fa fa-folder-open"></i> Detail</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer id="footer" class="py-5 bg-dark text-white">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h3>Tentang Kami</h3>
+					<p><b>Inpo Loker</b> merupakan Lorem, ipsum dolor, sit amet consectetur adipisicing elit. Laudantium cum sapiente modi illum asperiores perspiciatis quisquam, incidunt magni rerum quia. Asperiores fugiat harum iste officia non. Nihil aliquam veritatis odit.</p>
+					<p>Hak Cipta &copy; 2023 | Inpo Loker</p>
+				</div>
+				<div class="col-md-6">
+					<h3>Ikuti Kami</h3>
+					<div class="d-flex">
+						<a class="m-3 text-white fs-3" href=""><i class="fab fa-facebook"></i></a>
+						<a class="m-3 text-white fs-3" href=""><i class="fab fa-twitter"></i></a>
+						<a class="m-3 text-white fs-3" href=""><i class="fab fa-instagram"></i></a>
+						<a class="m-3 text-white fs-3" href=""><i class="fab fa-youtube"></i></a>
+						<a class="m-3 text-white fs-3" href=""><i class="fab fa-linkedin"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
