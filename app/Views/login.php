e36fde0d99525e606a871d21f3b9401244c78a4a
hendikaseptio <hendikaseptio11@gmail.com>
2023-02-22 17:57:01 +0700

    Membuat halaman Login

---
+++ app/Views/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login | Inpo Loker</title>
+	<link rel="stylesheet" href="/assets/css/custom.css">
+	<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
+</head>
+<body>
+	<div class="section" id="login">
+		<div class="container h-100">
+			<div class="row align-items-center h-100">
+				<div class="col-md-6 col-9 m-auto">
+					<div class="card shadow-lg rounded-4">
+						<div class="card-body py-4 px-4">
+							<form action="">
+								<div class="text-center">
+									<i class="fs-1 fa fa-handshake"></i>
+									<h1 class="text-center mb-4">INPO LOKER</h1>
+									<hr>
+								</div>
+								<div class="mb-3">
+									<label for="username" class="form-label">Username</label>
+									<input type="text" name="username" id="username" class="form-control" placeholder="Masukkan username">
+								</div>
+								<div class="mb-3">
+									<label for="password" class="form-label">Password</label>
+									<input type="password" name="password" id="password" class="form-control" placeholder="Masukkan password">
+								</div>
+								<div class="d-flex justify-content-between">
+									<button type="submit" class="btn rounded-pill btn-outline-dark"><i class="fa fa-reply"></i> Kembali</button>
+									<button type="submit" class="shadow btn rounded-pill btn-primary"><i class="fa fa-sign-in-alt"></i> Masuk</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
